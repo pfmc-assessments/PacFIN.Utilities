960659d5ceb46b812f6a5d1624f831a15780a44e
kellijohnson <kellifayejohnson@gmail.com>
2015-06-16 11:09:54 -0700

    Many changes to plotCleaned:
    Allow either ages or lengths to be ploted if length = FALSE or TRUE.
    Only print species name on outer main title.
    Change axes names and if all axes are printed to reduce space.
    Use cm instead of mm.
    Move legends to where there is likely to be less data.
    Remove the box around legends.
    Include zero in ylimits to make Andre happy.
    Move plots around to have groupings on left and data on right.

--- R/plotCleaned.R
+++ R/plotCleaned.R
@@ -1,5 +1,4 @@
-plotCleaned = function (Pdata) {
-
+plotCleaned = function (Pdata, length = TRUE) {
   SPID = sort(unique(Pdata$SPID))
 
   geargroups <- ifelse(length(Pdata$geargroup) > 0, TRUE, FALSE)
@@ -7,37 +6,46 @@ plotCleaned = function (Pdata) {
   par(mfrow = c(ifelse(geargroups, 3, 2), 2),
     oma = c(1, 1, 3, 0.25), mar = c(0.5, 3.25, 0, 0),
     mgp = c(1.5, 0.5, 0))
+
   # Number of lengths by state per year
-
-  lenData = Pdata[Pdata$length > -1,]
-  barplot(xtabs(lenData$FREQ ~ Pdata$state + Pdata$fishyr), col=rainbow(3),
-          legend.text = T, xlab="Year", ylab = "Length Samples",
-          main=paste("Species", SPID))
-
+  if (length) {
+    Pdata <- Pdata[Pdata$length > -1, ]
+    ylim2 <- c(0, max(Pdata$lengthcm, na.rm = TRUE))
+  } else {
+    ylim2 <- c(0, max(Pdata$age, na.rm = TRUE))
+  }
+  barplot(xtabs(Pdata$FREQ ~ Pdata$state + Pdata$fishyr), col = rainbow(3),
+    legend.text = TRUE, xaxt = "n", xlab = "", ylab = "Samples per state",
+    args.legend = list(x = "topleft", bty = "n"))
+  mtext(side = 3, outer = TRUE, line = 1,
+    paste(paste(SPID, collapse = ","), "Fishery", ifelse(length, "length", "age"),
+    "compositions"))
  # Lengths
 
-  boxplot(lenData$length ~ Pdata$fishyr, ylab="Lengths",
-          main=paste("Species", SPID))
+  boxplot(Pdata$lengthcm ~ Pdata$fishyr,
+    ylab = ifelse(length, "Length (cm)", "Age"), xaxt = "n",
+    frame.plot = FALSE, ylim = ylim2)
 
   # if (.Platform$OS.type == "unix") { quartz() } else { x11() }
 
   nGRID = length(unique(Pdata$GRID))
 
   barplot(xtabs(Pdata$FREQ ~ Pdata$GRID + Pdata$fishyr), col=rainbow(nGRID),
-          legend.text = T, xlab = "Year", ylab = "Gears",
-          main=paste("Species", SPID))
+    legend.text = TRUE, xaxt = ifelse(geargroups, "n", "t"),
+    ylab = "Samples per gear",
+    args.legend = list(x = "topleft", bty = "n", ncol = ceiling(nGRID / 4)))
 
-  if ( length(Pdata$geargroup) > 0 ) {
+  boxplot(Pdata$DEPTH_AVG ~ Pdata$fishyr, ylab = expression(bar(Depth)),
+    frame.plot = FALSE, ylim = c(0, max(Pdata$DEPTH_AVG, na.rm = TRUE)))
+
+  if (geargroups) {
 
     nGG = length(unique(Pdata$geargroup))
 
     barplot(xtabs(Pdata$FREQ ~ Pdata$geargroup + Pdata$fishyr), col=rainbow(nGG),
-            legend.text = T, xlab = "Year", ylab = "Gear Groups",
-            main=paste("Species", SPID))
+      legend.text = T, xlab = "", ylab = "Samples per gear group",
+      args.legend = list(x = "topleft", bty = "n"))
 
   } # End if
-
-  boxplot(Pdata$DEPTH_AVG ~ Pdata$fishyr, ylab="AVG_DEPTH",
-          main=paste("SPECIES", SPID))
 
 } # End function plotCleaned
