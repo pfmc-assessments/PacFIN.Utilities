050c811389c6df17f97c9f8e376c7a0440adea34
kellijohnson <kellifayejohnson@gmail.com>
2015-06-19 09:20:22 -0700

    Update to include what parameter is what in weightlength docs

--- man-roxygen/weightlengthparams.R
+++ man-roxygen/weightlengthparams.R
@@ -1,6 +1,6 @@
-#' @param fa Female specific weight length.
-#' @param fb Female specific weight length.
-#' @param ma Male specific weight length.
-#' @param mb Male specific weight length.
-#' @param ua Unsexed weight length.
-#' @param ub Unsexed weight length.
+#' @param fa Female specific weight length coefficient.
+#' @param fb Female specific weight length exponent.
+#' @param ma Male specific weight length coefficient.
+#' @param mb Male specific weight length exponent.
+#' @param ua Unsexed weight length coefficient.
+#' @param ub Unsexed weight length exponent.
