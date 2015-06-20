46f89be728f60419b03606c95ee26fad8ab3f5b6
kellijohnson <kellifayejohnson@gmail.com>
2015-06-20 06:47:34 -0700

    Change to roxygen and add example

--- R/capValues.R
+++ R/capValues.R
@@ -1,19 +1,25 @@
-##############################################################################
-#
-# capValues takes a data column and a capping factor, which may be either
-# a quantile (less than 1), or a value (greater than 1), and reduces outsize
-# values in the data to the value given, or to the value of the dataset at
-# that quantile.
-#
-# For example, the default 0.95 quantile in a dataset with maximum value 10
-# might be 8.5.  Then all values greater than 8.5 will be reduced to 8.5.
-#
-# If a value is given, for example 9.3, then all values greater than 9.3
-# will be reset to 9.3.
-#
-# A copy of the input data column with the modified values is returned.
-#
-##############################################################################
+#' Reset all values greater than a maximum specified value.
+#'
+#' \code{capValues} takes a vector and will return a vector where all
+#' values greater than some specified maximum are reset at that maximum
+#' value. The maximum (\code{maxVal}) may be specified as either a
+#' quantile or a true numeric value.
+#'
+#' @param DataCol A vector of numeric values.
+#' @param maxVal A numeric value specifying the quantile or value to cap
+#'   all data at. The default value is \code{0.95}, which caps all values
+#'   in \code{DataCol} at the 95 percent quantile. If a numeric value
+#'   greater than one is specified then all values in \code{DataCol} greater
+#'   than the specified value will be reset to \code{maxVal}. If \code{maxVal}
+#'   is less than one it assumes a quantile is specified.
+#' @return A vector of numeric values where no value is greater than the
+#'   maximum specified by \code{maxVal}.
+#' @example
+#' x <- 1:10
+#' y <- capValues(x, 5)
+#' z <- capValues(x, 0.75)
+#' rbind(x, y, z)
+#' @author Andi Stephens
 
 capValues = function( DataCol, maxVal=0.95 ) {
 
