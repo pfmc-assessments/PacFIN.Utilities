82d692094a7a44501759aed7c2bbe91548bed44d
kellijohnson <kellifayejohnson@gmail.com>
2015-06-20 13:20:10 -0700

    Start adding column information

--- man-roxygen/Pdata.R
+++ man-roxygen/Pdata.R
@@ -1,2 +1,4 @@
 #' @param Pdata A \code{data.frame} originally extracted
 #'   from PacFIN and used to generate composition data.
+#'   The \code{data.frame} contains many columns:
+#'   \code{TOTAL_WGT} is the weight of the final resorting;
