388615b81cc3e6b05af45f2487385d1160697bdf
Andi Stephens <Andi.Stephens@noaa.gov>
2016-11-10 16:13:34 -0800

    Knew it.

---
+++ R/PacFIN.Utilities.R
@@ -0,0 +1,84 @@
+#' PacFIN.Utilities: Functions for working up PacFIN biological (BDS) data.
+#'
+#' The PacFIN.Utilites package provides functions for filtering, summarizing, expanding, and compiling 
+#' composition data, and writing out the final products:  length- age- and age-at-length
+#' compositions.
+#' 
+#' Many of the functions described below write summary output to the console, and several create plots for 
+#' visualizing data at various steps in processing.  The information written to the console
+#' can be captured using the \code{\link{sink}} function to encapsulate your workflow:
+#' 
+#' \code{sink("myfilename.txt", split=TRUE)}
+#' 
+#' ... do your work ...
+#' 
+#' \code{sink()}
+#' 
+#' The second call closes the file.
+#' 
+#' @section Workflow:
+#' There are 15 functions in this package, and they are sensitive to the order in which they
+#' are run.  You can see diagram of the workflow you can expect to follow
+#' \code{\link{Workflow}}, as well as additional notes.
+#' 
+#' 
+#' @section Filtering functions:
+#' \tabular{lcl}{
+#' \code{\link{cleanPacFIN}}\tab\tab Filters raw PacFIN data\cr
+#' \code{\link{cleanAges}}\tab\tab Filters for age data\cr
+#' }
+#' 
+#' @section Summary functions:
+#' \tabular{lcl}{
+#' \code{\link{plotRawData}}\tab\tab Plots commonly viewed raw PacFIN data\cr
+#' \code{\link{plotCleaned}}\tab\tab Plots the product of \code{cleanPacFIN} similarly.\cr
+#' }
+#' 
+#' In addition, the \code{\link{getExpansion_1}} function has a "plot" argument to create
+#' plots documenting the expansions it creates.  It also writes summary output to the
+#' console.
+#' 
+#' @section Optional functions:
+#' \tabular{lcl}{
+#' \code{\link{combineCalCOM}}\tab\tab For combinining PacFIN and CalCOM data\cr
+#' \code{\link{setSeason}}\tab\tab For treating data from seasonal fisheries, such as Petrale.\cr
+#' \code{\link{Stratify}}\tab\tab Reduces the values in a data column to an encoded or named subset.\cr
+#' }
+#' 
+#' @section Expansion functions:
+#' \tabular{lcl}{
+#' \code{\link{getExpansion_1}}\tab\tab creates the values for the sample to tow expansion.\cr
+#' \code{\link{getExpansion_2}}\tab\tab computes the expansion values from the tow upwards to a\cr
+#' \tab\tab user-specified stratification
+#' }
+#' 
+#' @section Expansion caveats:
+#' There is one manual step in the workflow.
+#' After running the expansion functions, data columns Expansion_Factor_1 and 
+#' Expansion_Factor_2 are available to use in manually setting the Final_Expansion_Factor. 
+#' \itemize{
+#' \item{Age data are expanded separately from lengths, after running \code{cleanAges}}.
+#' \item{WA fish are generally only expanded using Expansion_Factor_2.}
+#' \item{Other expansions are the product of Expansion_Factor_1 * Expansion_Factor_2}
+#' \item{For age-at-length comps, set Final_Expansion_Factor to 1.  Each fish represents only itself.}
+#' }
+#' 
+#' @section Compostition functions:
+#' \tabular{lcl}{
+#' \code{\link{getComps}}\tab\tab uses the Final_Sample_Size and the user-specified 
+#' stratification to create comps.\cr
+#' \code{\link{applySexRatio}}\tab\tab separates the unsexed fish into males and females\cr
+#' \code{\link{writeComps}}\tab\tab formats the composition data for SS3 and writes it to a file.\cr
+#' }
+#' 
+#' @section Infrequentily used functions:
+#' You can run these manually to reset the inital values.
+#' \tabular{lcl}{
+#' \code{\link{getState}}\tab\tab Run by \code{cleanPacFIN} to initialized the \code{state} column.\cr
+#' \code{\link{getGearGroup}}\tab\tab Run by \code{cleanPacFIN} to initialize \code{geargroups}.\cr
+#' \code{\link{capValues}}\tab\tab Used to limit the maximum value in a vector.\cr
+#' }
+#'  
+#' @docType package
+#' @name PacFIN.Utilities
+NULL
---
+++ R/Workflow.R
@@ -0,0 +1,53 @@
+#' 
+#' @title Workflow
+#' 
+#' @name Workflow
+#' 
+#' @author Andi Stephens, Kelli Johnson
+#' 
+#' @description 
+#' 
+#' Illustrated workflow for PacFIN biological data (BDS) workup.
+#' 
+#' @details
+#' 
+#' The diagram below is a flowchart for working up PacFIN data. 
+#' 
+#' The PacFIN data should be acquired from John Wallace or Andi Stephens.  If you are using 
+#' CalCOM data, check with the Southwest Center Assessment team to find out who can provide
+#' that data.
+#' 
+#' If your data are in a \code{.csv} file, read them in using:
+#' \itemize{
+#' \item{\code{read.csv("filename.csv", as.is=T)}}
+#' }
+#' If your data are in a \code{.dmp} file, read them in using:
+#' \itemize{
+#' \item{\code{load("filename.dmp")}}
+#' }
+#' 
+#' 
+#' @section Expansion caveats:
+#' There is one manual step in the workflow. 
+#' After running the expansion functions, data columns Expansion_Factor_1 and 
+#' Expansion_Factor_2 are available to use in manually setting the Final_Expansion_Factor. 
+#' \itemize{
+#' \item{Age data are expanded separately from lengths, after running \code{cleanAges}.}
+#' \item{WA fish are generally only expanded using Expansion_Factor_2.}
+#' \item{Other expansions are the product of Expansion_Factor_1 * Expansion_Factor_2}
+#' \item{For age-at-length comps, set Final_Expansion_Factor to 1.  Each fish represents only itself.}
+#' }
+#' 
+#' @section Workflow: {
+#' 
+#' In the diagram below, datasets are represented as circles or ellipses, functions are denoted
+#' by rectangles, and black and red lines represent workflow for generating age, length, or
+#' age-at-length comps.  Optional paths are represented by dashed lines.
+#' 
+#'
+#' \if{html}{\figure{Full-Workflow.png}{options: alt="Figure: Simple-Workflow.png"}}
+#' \if{latex}{\figure{Full-Workflow.png}{options: width=7cm}}
+#' }
+#' 
+#' 
+NULL
