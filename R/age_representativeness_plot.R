13869b0cb4b3943d1a00caeb9e6a9a8c43d40ffe
Ian Taylor <ian.taylor@noaa.gov>
2020-08-13 12:34:18 -0700

    style: run styler::style_file() on age_representativeness_plot.R

--- R/age_representativeness_plot.R
+++ R/age_representativeness_plot.R
@@ -1,5 +1,5 @@
 #' Compare length historgram of aged samples to all lengthed samples
-#' 
+#'
 #' @param bio The dataframe containing the lengths and ages. Currently the
 #' function works with data from the West Coast Groundfish Bottom Trawl
 #' Survey extracted by the nwfscSurvey package as well as PacFIN data.
@@ -9,7 +9,7 @@
 #' @param xlim The x limits for the histogram
 #' @param ylim The y limits for the histogram
 #' @param max_break The maximum length in cm (If PacFIN data is provided,
-#' FISH_LENGTH is automatically converted from mm to cm). 
+#' FISH_LENGTH is automatically converted from mm to cm).
 #' @param file The relative or absolute path and name of the file to write to,
 #'  as a .png file.
 #' @param plot_panels A vector of two integers that determine the matrix
@@ -25,69 +25,74 @@
 #' @export
 #' @author Ian Taylor, Kathryn Doering, Brian Langseth, Kelli Johnson
 #' @examples
-#'   data(XMPL.BDS, package = "PacFIN.Utilities")
-#'   state_codes <- c("W", "O", "C")
-#'   names(state_codes) <- c("WA", "OR", "CA") # just for clarity
-#'   for (i in state_codes) {
-#'     age_representativeness_plot(XMPL.BDS[XMPL.BDS$SOURCE_AGID == i,],
-#'                                 file = paste0( "agelength_bds_", i ,".png"),
-#'                                 ylim = c(0, 0.1)) # trial and error for now to figure out
+#' data(XMPL.BDS, package = "PacFIN.Utilities")
+#' state_codes <- c("W", "O", "C")
+#' names(state_codes) <- c("WA", "OR", "CA") # just for clarity
+#' for (i in state_codes) {
+#'   age_representativeness_plot(XMPL.BDS[XMPL.BDS$SOURCE_AGID == i, ],
+#'     file = paste0("agelength_bds_", i, ".png"),
+#'     ylim = c(0, 0.1)
+#'   ) # trial and error for now to figure out
 #' }
-age_representativeness_plot <- function(bio.WCGBTS,
+age_representativeness_plot <- function(bio,
                                         xlim = c(0, max_break),
                                         ylim = c(0, 0.049),
                                         max_break = 155,
                                         file = NULL,
                                         plot_panels = c(10, 2),
-                                        wait2plot = FALSE){
-
+                                        wait2plot = FALSE) {
   if (!is.null(file)) {
     on.exit(dev.off(), add = TRUE)
   }
   if (!is.null(file)) wait2plot <- FALSE
 
-  bio.WCGBTS <- changecol_pacfin(bio.WCGBTS)
-  bio.WCGBTS <- bio.WCGBTS[!(is.na(bio.WCGBTS$Age) &&  is.na(bio.WCGBTS$Length_cm)), ]
-  if (nrow(bio.WCGBTS) == 0) {
+  bio <- changecol_pacfin(bio)
+  bio <- bio[!(is.na(bio$Age) && is.na(bio$Length_cm)), ]
+  if (nrow(bio) == 0) {
     stop("Your data does not have any length or age data.")
   }
-  if (any(bio.WCGBTS[, "Length_cm"] >= max_break, na.rm = TRUE)) {
-    nrows_old <- nrow(bio.WCGBTS)
-    bio.WCGBTS <- bio.WCGBTS[bio.WCGBTS[, "Length_cm"] < max_break, ]
-    warning(nrows_old - nrow(bio.WCGBTS),
-            " out of ",
-            nrows_old,
-            " length values are larger than max_break and will be excluded.")
+  if (any(bio[, "Length_cm"] >= max_break, na.rm = TRUE)) {
+    nrows_old <- nrow(bio)
+    bio <- bio[bio[, "Length_cm"] < max_break, ]
+    warning(
+      nrows_old - nrow(bio),
+      " out of ",
+      nrows_old,
+      " length values are larger than max_break and will be excluded."
+    )
   }
-  years <- sort(unique(bio.WCGBTS$Year))
+  years <- sort(unique(bio$Year))
 
   colvec <- c(rgb(1, 0, 0, alpha = 0.8), rgb(0, 0, 1, alpha = 0.5))
 
-  pc <- 1 #plot counter
+  pc <- 1 # plot counter
   for (y in years) {
-    
-    mod <- (y-years[1]) %%  (prod(plot_panels)) #modulus to determine if plot if filled
+    mod <- (y - years[1]) %% (prod(plot_panels)) # modulus to determine if plot if filled
     if (!is.null(file) && mod == 0) {
-      if(pc != 1) dev.off()
-      file_name <- strsplit(file,"[.]")[[1]][1]
-      file_ext <- strsplit(file,"[.]")[[1]][2]
-      png(filename = paste0(file_name,"_",pc,".",file_ext), width = 7, height = 7, units = 'in', res = 300)
+      if (pc != 1) dev.off()
+      file_name <- strsplit(file, "[.]")[[1]][1]
+      file_ext <- strsplit(file, "[.]")[[1]][2]
+      png(filename = paste0(file_name, "_", pc, ".", file_ext), width = 7, height = 7, units = "in", res = 300)
       pc <- pc + 1
-      
+
       # make multi-panel plot comparing length samples to the subset with ages
-      par(mfcol = plot_panels,
-          mar = c(0.2,0.2,0.2,0.2),
-          oma = c(4,4,1,1))
+      par(
+        mfcol = plot_panels,
+        mar = c(0.2, 0.2, 0.2, 0.2),
+        oma = c(4, 4, 1, 1)
+      )
     }
-    if (is.null(file)) par(oma = c(1,1,1,1), ask = wait2plot)
-    
+    if (is.null(file)) par(oma = c(1, 1, 1, 1), ask = wait2plot)
+
     # make empty plot (note: xlim and ylim were set by trial and error)
-    plot(0, type = 'n',
-         xlim = xlim,
-         xaxs = 'i', xlab = "",
-         ylim = ylim,
-         yaxs = 'i', ylab = "",
-         axes = FALSE)
+    plot(0,
+      type = "n",
+      xlim = xlim,
+      xaxs = "i", xlab = "",
+      ylim = ylim,
+      yaxs = "i", ylab = "",
+      axes = FALSE
+    )
     grid()
     if (par()$mfg[2] == 1) {
       axis(2, las = 1)
@@ -95,62 +100,77 @@ age_representativeness_plot <- function(bio.WCGBTS,
     if (par()$mfg[1] == par()$mfg[3] | y == max(years)) {
       axis(1)
     }
-    lengths.y <- bio.WCGBTS$Length_cm[bio.WCGBTS$Year == y]
-    ages.y <- bio.WCGBTS$Length_cm[bio.WCGBTS$Year == y &
-                                        !is.na(bio.WCGBTS$Age)]
+    lengths.y <- bio$Length_cm[bio$Year == y]
+    ages.y <- bio$Length_cm[bio$Year == y &
+      !is.na(bio$Age)]
     lhist <- hist(lengths.y,
-         breaks = seq(0, max_break, 5),
-         freq = FALSE,
-         col = colvec[1],
-         add = TRUE)
+      breaks = seq(0, max_break, 5),
+      freq = FALSE,
+      col = colvec[1],
+      add = TRUE
+    )
     if (length(ages.y > 0)) {
       ahist <- hist(ages.y,
-           breaks = seq(0, max_break, 5),
-           freq = FALSE,
-           col = colvec[2],
-           add = TRUE)
+        breaks = seq(0, max_break, 5),
+        freq = FALSE,
+        col = colvec[2],
+        add = TRUE
+      )
     }
     p.value <- NA
-    p.color <- 'grey50'
+    p.color <- "grey50"
     bhat <- NA
     if (length(lengths.y) > 0 & length(ages.y) > 0) {
-      p.value <- suppressWarnings(classes = "warning",
-        ks.test(x = lengths.y, y = ages.y)$p.value)
-      p.color <- ifelse(p.value > 0.05, 'green3', 'red')
-      bhat <- sum(sqrt(lhist$counts/sum(lhist$counts)*ahist$counts/sum(ahist$counts)))
+      p.value <- suppressWarnings(
+        classes = "warning",
+        ks.test(x = lengths.y, y = ages.y)$p.value
+      )
+      p.color <- ifelse(p.value > 0.05, "green3", "red")
+      bhat <- sum(sqrt(lhist$counts / sum(lhist$counts) * ahist$counts / sum(ahist$counts)))
     }
-    legend('topleft', legend = NA, bty = 'n', title = y, cex = 1.5)
+    legend("topleft", legend = NA, bty = "n", title = y, cex = 1.5)
     myinset <- ifelse(is.null(file), 0, -0.25)
     myinset2 <- ifelse(is.null(file), -0.02, -0.35)
-    legend('bottomright', legend = "\n\n", bty = 'n', inset = c(0, myinset),
-           title = paste0(length(lengths.y), " lengths\n",
-                          length(ages.y), " ages (",
-                          round(100*length(ages.y)/length(lengths.y)),
-                          "%)"),
-           cex = 1.0)
-    legend('bottomright', legend = "\n", bty = 'n', inset = c(0, myinset),
-           title = paste0("K-S p-value = ",
-                          sprintf("%.3f", p.value)),
-           cex = 1.0, title.col = p.color)
-    legend('bottomright',legend = NA, bty = 'n', inset = c(0, myinset2),
-           title = as.expression(bquote(atop(phantom(),hat(b)==.(sprintf("%.3f",bhat))))),
-           cex = 1.0, title.col = "black")
+    legend("bottomright",
+      legend = "\n\n", bty = "n", inset = c(0, myinset),
+      title = paste0(
+        length(lengths.y), " lengths\n",
+        length(ages.y), " ages (",
+        round(100 * length(ages.y) / length(lengths.y)),
+        "%)"
+      ),
+      cex = 1.0
+    )
+    legend("bottomright",
+      legend = "\n", bty = "n", inset = c(0, myinset),
+      title = paste0(
+        "K-S p-value = ",
+        sprintf("%.3f", p.value)
+      ),
+      cex = 1.0, title.col = p.color
+    )
+    legend("bottomright",
+      legend = NA, bty = "n", inset = c(0, myinset2),
+      title = as.expression(bquote(atop(phantom(), hat(b) == .(sprintf("%.3f", bhat))))),
+      cex = 1.0, title.col = "black"
+    )
     if ((par()$mfg[1] == plot_panels[1] & par()$mfg[2] == plot_panels[2]) |
-        is.null(file) | y == max(years)) {
+      is.null(file) | y == max(years)) {
       # empty plot for legend
       par.old <- par(no.readonly = TRUE)
       par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
       plot(0, 0, type = "n", bty = "n", xaxt = "n", yaxt = "n")
-      legend('bottomright', xpd = TRUE, inset = c(0, -0.01), horiz = FALSE,
-             bty = 'n',
-             fill = colvec,
-             cex = 1.0,
-             legend = c("All lengths", "Lengths with ages"))
+      legend("bottomright",
+        xpd = TRUE, inset = c(0, -0.01), horiz = FALSE,
+        bty = "n",
+        fill = colvec,
+        cex = 1.0,
+        legend = c("All lengths", "Lengths with ages")
+      )
       mtext("Length (cm)", side = 1, line = -1.0, outer = FALSE)
       par(par.old)
     }
   }
-
 }
 
 
@@ -180,8 +200,10 @@ changecol_pacfin <- function(data, verbose = FALSE) {
     }
     # Code other options for year column here
     if (!"Year" %in% colnames(data)) {
-      stop("A year column was not found in your data\n",
-        "Year or SAMPLE_YEAR work.")
+      stop(
+        "A year column was not found in your data\n",
+        "Year or SAMPLE_YEAR work."
+      )
     }
   }
   # Length column
@@ -191,8 +213,10 @@ changecol_pacfin <- function(data, verbose = FALSE) {
       data[, "Length_cm"] <- data[, "FISH_LENGTH"] * 0.1
     }
     if (!"Length_cm" %in% colnames(data)) {
-      stop("A fish-length column was not found in your data\n",
-        "Length_cm or FISH_LENGTH work.")
+      stop(
+        "A fish-length column was not found in your data\n",
+        "Length_cm or FISH_LENGTH work."
+      )
     }
   }
   # Age column
@@ -201,8 +225,10 @@ changecol_pacfin <- function(data, verbose = FALSE) {
       data[, "Age"] <- data[, "FISH_AGE_YEARS_FINAL"]
     }
     if (!"Age" %in% colnames(data)) {
-      stop("A fish-age column was not found in your data\n",
-        "Age or FISH_AGE_YEARS_FINAL work.")
+      stop(
+        "A fish-age column was not found in your data\n",
+        "Age or FISH_AGE_YEARS_FINAL work."
+      )
     }
   }
   return(data)
