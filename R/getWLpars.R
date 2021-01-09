039444e8c7fe52b2f2912f667573998871cda8fc
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2021-01-09 05:51:43 -0800

    feat(getWLpars): return row of NA for model that doesn't converge
    Error was generated before, now when there isn't enough data the model
    will return a vector (or row) of NA values. This happens for example
    when there are no female fish with weights and lengths.
    The documentation was increased to reflect these changes and explain
    the output.

--- R/getWLpars.R
+++ R/getWLpars.R
@@ -5,6 +5,11 @@
 #'
 #' @param data A data frame containing empirical weights and lengths
 #' from sampled fish.
+#' Weights should be available in the column `weight` or `FISH_WEIGHT`.
+#' Lengths should be available in the column `lengthcm`.
+#' Sexes should be available in the column `sex` or `SEX`.
+#' All column names will be changed to lower case before searching for
+#' the appropriate indexing.
 #' @template verbose
 #'
 #' @author Kelli Faye Johnson
@@ -13,12 +18,18 @@
 #' Parameters A and B are in the appropriate units to input
 #' into Stock Synthesis Wtlen_1_Fem and Wtlen_2_Fem, or
 #' Wtlen_1_Mal and Wtlen_1_Mal, parameters in the
-#' control file.
+#' control file. Values of `NA` are returned for models that
+#' did not have enough data to properly estimate the parameters.
+#' This will happen when there are no females in your data set,
+#' for example.
 #'
 getWLpars <- function(data, verbose = FALSE) {
 
   getline <- function(model){
     # function to get row of table of weight-length values
+    if (is.null(model)) {
+      return(c(median_intercept = NA, SD = NA, A = NA, B = NA))
+    }
     Amed <- exp(model$coefficients[1])
     B <- model$coefficients[2]
     sdres <- sd(model$residuals)
@@ -29,6 +40,24 @@ getWLpars <- function(data, verbose = FALSE) {
       "A" = Amean,
       "B" = B))
     names(out) <- c("median_intercept", "SD", "A", "B")
+    return(out)
+  }
+
+  lmgrowth <- function(sex, thedata = data, verbose = verbose) {
+    data.model <- thedata[thedata$sex %in% sex, ]
+    out <- tryCatch(
+      {
+        stats::lm(log(weight) ~ log(length_cm),
+          data = data.model)
+      },
+      error = function(cond) {
+        if (verbose) {
+          message("Too few data points for sex = ",
+            "c(", paste0("'", sex, "'", collapse = ", "), ")")
+        }
+        return(NULL)
+      }
+    )
     return(out)
   }
 
@@ -50,11 +79,7 @@ getWLpars <- function(data, verbose = FALSE) {
       nrow(data), "\nfish because ", dims[1] - nrow(data),
       " fish did not have empirical weights and lengths.")
   }
- 
-  lmgrowth <- function(sex, thedata = data) {
-    stats::lm(log(weight) ~ log(length_cm),
-        data = thedata[thedata$sex %in% sex, ])
-  }
+
   mresults <- lapply(list(
     females = "F",
     males = "M",
