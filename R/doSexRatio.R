562dcbb9a2865ad7ff7609961c384b8760f8ea45
Chantel Wetzel <chantel.wetzel@noaa.gov>
2021-02-18 11:19:49 -0800

    MAJOR overhaul: completely update logic and application of sex ratios

--- R/doSexRatio.R
+++ R/doSexRatio.R
@@ -16,50 +16,13 @@
 #' @export
 #'
 #' @param CompData data that have already been aggregated by \code{getComps}.
-#' @param findRatio logical.  Default: FALSE.  If TRUE, use the observed per-stratum ratio
-#' of F:M to assign the unsexed fish.
-#' @param Rvector sex ratios to apply to the given Bins, or a single ratio applie to all.
-#' @param Bins Bins (age or length) corresponding to Rvector
 #' @param ratioU the ratio to apply to fish less than maxsizeU
 #' @param maxsizeU the size below which the sex ratio is assumed ratioU.
 #' @param GTsizeU the size above which the ratio is assumed to be 1.0 (big mamas).
+#' @template savedir
 #'
 #' @details
 #' Sex ratios may be assigned in one of four different ways.
-#' 
-#' The default is to use a single-valued Rvector (0.5 or user-set) as the ratio to use for all
-#' unsexed fish.
-#' \itemize{
-#' \item{\code{C1 = doSexRatio(CompData)}}
-#' }
-#' 
-#' If findRatio is TRUE, then the observed per-stratum ratio of females to males is used for 
-#' the usexed fish in each stratum, and no other parameters apply.
-#' \itemize{
-#' \item{\code{C1 = doSexRatio(Compdata, findRatio=T)}}
-#' }
-#' 
-#' If maxsizeU is set, then all fish below that size  will be set to the value in
-#' ratioU.  If GTsizeU is set, then all unsexed fish above that size will be female.
-#' 
-#' In this example, the ratio for fish less than 3 is 0.5, for fish greater than 12 is 1,
-#' and for the fish in between is 0.6.
-#' 
-#' \itemize{
-#' \item{\code{C1 = doSexRatio(Compdata, Rvector=0.6, ratioU=0.5, maxsizeU=3, GTsizeU=12)}
-#' }}
-#' 
-#' If bins are used, they are assumed to be the length or age bins (for AGE comps) corresponding
-#' to the vector of ratios (unused for a single ratio).  If Age-at-Length comps, the bins are
-#' Lengths.
-#'
-#' In this example, lengths (ages) below the lowest Bin will fall into the
-#' lowest bin; i.e., lengths from 0-15 will have ratio .5, and fish above 36 will have ratio 
-#' 0.7 applied.
-#' 
-#' \itemize{
-#' \item{\code{C1 = doSexRatio(Compdata, Rvector=c(.5, .6, .7), Bins=c(10, 15, 36))}
-#' }}
 #'  
 #' @return
 #' 
@@ -70,200 +33,114 @@
 #' 
 #' @author
 #' 
-#' Andi Stephens, Kelli Johnson
+#' Andi Stephens, Kelli Johnson, and Chantel Wetzel
 #' 
 #' @seealso
 #' \code{\link{getComps}}, \code{\link{writeComps}}
 #'
 #############################################################################
 
-doSexRatio = function( CompData, findRatio=FALSE, Rvector=0.5, Bins=NULL,
-                       ratioU=.5, maxsizeU=0, GTsizeU=Inf ) {
-
+doSexRatio = function( CompData, ratioU, maxsizeU, GTsizeU, savedir ) {
 
   # If AGE comps, Bins are ages, not lengths.  Rename "age" to "lengthcm", then put
   # it back at the end!
-
   AGE_FLAG = FALSE
-
   if ( length(CompData$lengthcm) == 0 ) {
-
     index = which(names(CompData) == "age")
     names(CompData)[index] = "lengthcm"
-
     AGE_FLAG = TRUE
-
   } # End if
 
   # Fix arithmetic
-
-  CompList = c("male","msamps","mtows","female","fsamps","ftows","unsexed","usamps","ONLY_U_TOWS","alltows")
+  CompList = c("male", "msamps", "mtows",
+               "female", "fsamps", "ftows",
+               "unsexed", "usamps", "ONLY_U_TOWS", 
+               "alltows")
   tmp = CompData[,CompList]
   tmp[is.na(tmp)] = 0
   CompData[,CompList] = tmp
   
-  if ( GTsizeU != 0 & maxsizeU != 0 ) {
-    
-    if ( length(Rvector) > 1 ) { stop("Rvector expected to be a single ratio if you use GTsizeU") }
-    
-    # Create vectors from the appropriate values.
-    
-    Rvector = c(ratioU, Rvector, 1)
-    Bins = c(0, maxsizeU, GTsizeU)
-    
+  if (!missing(ratioU) & missing(maxsizeU)) {
+    stop("Error: The maxsizeU needs to be specified if you are using ratioU")    
   }
 
-  if ( findRatio ) {
 
-    cat("\nApplying per-stratum sex ratio.\n\n")
+  calcRatio <- prop.table(x = as.matrix(CompData[, c("female", "male")]), margin = 1)[, 1]
 
-    getRatio = function( x, y ) {
+  # Check to see if some years are only unsexed
+  CompData$percent_unsexed <- CompData$usamps / 
+   (CompData$usamps + CompData$fsamps + CompData$msamps)
+  check <- aggregate(percent_unsexed~fishyr, CompData, function(x) sum(x == 1) / sum(x) )
+  yrs <- check[,'percent_unsexed'] == 1 & is.finite(check[, 'percent_unsexed'])
+  n   <- sum(yrs, na.rm = TRUE)
+  if(n > 0 ) {
+    message("Thera are ", n, " years (",
+    unique(check[yrs,'fishyr']), ") with only unsexed fish. Applying sex ratio to these years may not be ideal.")
+  }   
 
-      tmp = ( x / ( x + y ))
-      tmp[x == 0 & y == 0] = 0.5
-      tmp[x != 0 & y == 0] = 1
+  # Deal with NaNs
+  noRatio <- which(!is.finite(calcRatio))
+  for(i in noRatio){
+    # 1st: Try to fill in based on neighboring lengths within a year
+    # Initially, I was also using fleet but appeared to lead to increased
+    # variability in the sex ratio being calculated.
+    nearLens <- c((CompData[i, 'lengthcm'] - 2):(CompData[i, 'lengthcm'] + 2))
+    ind <- which(CompData[, 'fishyr'] == CompData[i, 'fishyr'] & 
+                 CompData[ , 'lengthcm'] %in% nearLens )
+    # Only use if the observations are 3 or greater
+    # One will be the missing sex ratio, so a 3 or more would indicate more than
+    # one near neighbor observation
+    if (length(ind) >= 3 & sum(!is.finite(calcRatio[ind])) != length(ind)){
+      calcRatio[i] <- mean(calcRatio[ind], na.rm = TRUE)     
+    } else{
+        # 2nd: Use sex ratio for the same length across all data
+        ind <- which(CompData[, 'lengthcm'] == CompData[i,'lengthcm'])
+        calcRatio[i] <- mean(calcRatio[ind], na.rm = T)
+    }
+  }
 
-      return(tmp)
+  # Now let's check small fish if user has pre-specified ratio to be applied
+  if(!missing(maxsizeU)){
+    ind <- which(CompData[, 'usamps'] > 0 & CompData[, 'lengthcm'] < maxSizeU) 
+    if (length(ind) > 0){
+      calcRatio[ind] <- ratioU
+    }
+  }
 
-    } # End if findRatio
+  if(!missing(GTsizeU)){
+    ind <- which(CompData[, 'usamps'] > 0 & CompData[, 'lengthcm'] > GTsizeU)
+    if (length(ind) > 0){
+      calcRatio[ind] <- 1.0
+    }
+  }
 
-    # KFJ: If bins are supplied, only use the ratio for those bins
-    # that have a user supplied value of NA, this way a user can give
-    # values for lengths below a certain size and then the calculated
-    # ratio can be used for lengths greater than, or something along those
-    # lines.
+  CompData$sexRatio <- calcRatio
 
-    # Also instead of using your own function you can use
-    # calcRatio <- prop.table(CompData[, c("female", "male")], margin = 1)[, 1]
+  # Now let's apply the calcRatio to all the unsexed fish:
+  for ( i in 1:nrow(CompData) ) {
+    CompData$female[i] <- CompData$female[i] + CompData$unsexed[i] * calcRatio[i]
+    CompData$fsamps[i] <- CompData$fsamps[i] + CompData$usamps[i] * calcRatio[i]
+    CompData$ftows[i]  <- CompData$ftows[i]  + CompData$ONLY_U_TOWS[i] * calcRatio[i]
+    CompData$male[i]   <- CompData$male[i]   + CompData$unsexed[i] * ( 1 - calcRatio[i] )
+    CompData$msamps[i] <- CompData$msamps[i] + CompData$usamps[i] * ( 1 - calcRatio[i] )
+    CompData$mtows[i]  <- CompData$mtows[i]  + CompData$ONLY_U_TOWS[i] * ( 1 - calcRatio[i] )
+  } 
 
-    # Though I think a good addition would be to look at neighboring bins
-    # for a ratio rather than just assigning a value of 0.5 when there are
-    # no observations in a given bin. Maybe even use a hierarchy of 2 nearest
-    # bins, then same size but different year? Not sure on the best protocol here.
-    # Could look to https://github.com/nwfsc-assess/nwfscSurvey/blob/master/R/SS3LF.fn.R
-    # code for the sexRatio protocol to keep it consistent.
-
-    calcRatio = getRatio(CompData$female, CompData$male)
-
-    # If there are only unsexed in a stratum, need to replace NAs
-
-    calcRatio[is.na(calcRatio)] = 0.5
-
-    cat("\nSummary of sex ratios observed per stratum:\n\n")
-
-    print(summary(calcRatio))
-
-    # Now combine the calculated ratio with a user-supplied ratio
-
-    if (length(Rvector) > 1) {
-
-      Ratio <- Rvector[match(CompData$lengthcm, Bins)]
-      Ratio[is.na(Ratio)] <- calcRatio[is.na(Ratio)]
-
-    } else {
-
-      # Use the calculated ratio for all bins
-      Ratio <- calcRatio
-
-    } # End if-else
-
-    for ( i in 1:nrow(CompData) ) {
-
-      CompData$female[i] = CompData$female[i] + CompData$unsexed[i] * Ratio[i]
-
-      CompData$fsamps[i] = CompData$fsamps[i] + CompData$usamps[i] * Ratio[i]
-
-      CompData$ftows[i] = CompData$ftows[i] + CompData$ONLY_U_TOWS[i] * Ratio[i]
-
-      CompData$male[i] = CompData$male[i] + CompData$unsexed[i] * ( 1 - Ratio[i] )
-
-      CompData$msamps[i] = CompData$msamps[i] + CompData$usamps[i] * ( 1 - Ratio[i] )
-
-      CompData$mtows[i] = CompData$mtows[i] + CompData$ONLY_U_TOWS[i] * ( 1 - Ratio[i] )
-
-    } # End for
-
-    # If debugging:  CompData = cbind(CompData, Ratio)
-
-    return(CompData)
-
-  } # End if findRatio
-
-  if ( length(Rvector) > 1 ) {
-
-    if ( length(Bins) != length(Rvector) ) { stop("Rvector and Bins are not the same length") }
-
-    tmp = paste(Rvector, collapse = ",")
-
-    cat("\nApplying sex ratio:", tmp, "to numbers, samples and tows\n\n")
-
-    # Recode lengths to correspond to bins given
-
-    lens = findInterval(CompData$length, Bins, rightmost.closed=T)
-
-    # Should be unnecessary
-
-    if ( min(lens) == 0 ) {
-
-      lens[lens == 0] = 1
-      cat("Lengths/ages less than the lowest bin will be treated as belonging
-          to that bin")
-
-    } # End if
-
-    for ( i in sort(unique(lens)) ) {
-
-      CompData$female[lens == i] = CompData$female[lens == i] +
-                                   CompData$unsexed[lens == i] *
-                                   Rvector[i]
-
-      CompData$fsamps[lens == i] = CompData$fsamps[lens == i] +
-                                   CompData$usamps[lens == i] *
-                                   Rvector[i]
-
-      CompData$ftows[lens == i] = CompData$ftows[lens == i] +
-                                  CompData$ONLY_U_TOWS[lens == i] *
-                                  Rvector[i]
-
-      CompData$male[lens == i] = CompData$male[lens == i] +
-                                 CompData$unsexed[lens == i] *
-                                 (1 - Rvector[i])
-
-      CompData$msamps[lens == i] = CompData$msamps[lens == i] +
-                                   CompData$usamps[lens == i] *
-                                   (1 - Rvector[i])
-
-      CompData$mtows[lens == i] = CompData$mtows[lens == i] +
-                                  CompData$ONLY_U_TOWS[lens == i] *
-                                  (1 - Rvector[i])
-
-    } # End for
-
-  } else {
-
-    tmp = paste(Rvector, collapse = ",")
-    cat("\nApplying sex ratio:", tmp, "to numbers, samples and tows\n\n")
-
-    # apply a single ratio over all lengths
-
-    CompData$female = CompData$female + Rvector * CompData$unsexed
-    CompData$fsamps = CompData$fsamps + Rvector * CompData$usamps
-    CompData$ftows = CompData$ftows + Rvector * CompData$ONLY_U_TOWS
-
-    CompData$male = CompData$male + (1 - Rvector) * CompData$unsexed
-    CompData$msamps = CompData$msamps + (1 - Rvector) * CompData$usamps
-    CompData$mtows = CompData$mtows + (1 - Rvector) * CompData$ONLY_U_TOWS
-
-  } # End if-else
+  if (!missing(savedir)) {
+    find = which(CompData$usamps > 0 )
+    png(file.path(savedir, "Applied_Sex_Ratio_to_Unsexed_Fish.png"))
+      on.exit(dev.off(), add = TRUE)
+      plot(CompData[find,'lengthcm'], CompData[find, 'sexRatio'],
+        xlab = "Length (cm)", ylab = "Sex Ratio",
+        main = paste0("Sex Ratio Applied to Unsexed Fish (N = ", sum(CompData$usamps), ")" )
+      )
+  }
 
   # If AGE comps, Bins are ages, not lengths.  Rename "age" to "lengthcm", then put
   # it back at the end!
-
   if (AGE_FLAG) {
-
     index = which(names(CompData) == "lengthcm")
     names(CompData)[index] = "age"
-
   } # End if
 
   cat("\nDone.\n\n")
