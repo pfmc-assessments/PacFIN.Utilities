2a8337b1fb35da2d8f79e1ce5af366f47299ed11
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2022-12-01 10:05:59 -0800

    Removes ODFW sample from call

--- R/sql.bds.R
+++ R/sql.bds.R
@@ -1,31 +1,27 @@
-#' Write SQL Text for Composition Data
+#' Write SQL text for composition data
 #'
 #' Write SQL text as a single character string that will result in
 #' getting the composition data from the PacFIN database.
 #'
 #' @template pacfin_species_code
 #' @template returnsql
-#' @author John R. Wallace, Kelli F. Johnson
+#' @author Kelli F. Johnson
 #'
 sql.bds <- function(pacfin_species_code) {
-  spid <- paste0("('", paste(pacfin_species_code, collapse = "','"), "')")
+  spid <- glue::glue("('{{{ pacfin_species_code }}}')")
+  # Consider changing 
+  # * VESSEL_NUM to VESSEL_ID b/c less NULL
+  # * TOTAL_WGT, might be spp weight
+  # * WGTMAX, i don't think this one matters
+  # * WGTMIN, i don't think this one matters
+  # * all_cluster_sum, done with ave
   sqlcall <- paste0(
-  "Select s.*,
-          o.UNK_NUM,
-          o.UNK_WT
- ",
-  # Consider changing VESSEL_NUM to VESSEL_ID b/c less NULL
-          # xxx TOTAL_WGT, might be spp weight
-          # xxx WGTMAX, i don't think this one matters
-          # xxx WGTMIN, i don't think this one matters
-          # xxx all_cluster_sum, done with ave
+    "Select s.*",
    "from
-          pacfin_marts.COMPREHENSIVE_BDS_COMM s,
-          pacfin.bds_sample_odfw o
+          pacfin_marts.COMPREHENSIVE_BDS_COMM s
    where
-              s.PACFIN_SPECIES_CODE = any ", spid, "  
-          and s.SAMPLE_NUMBER = o.SAMPLE_NO(+)
-          and s.SAMPLE_YEAR = o.SAMPLE_YEAR(+)")
+          s.PACFIN_SPECIES_CODE = any ", spid
+  )
   sqlcall <- gsub("\\n", "", sqlcall)
-   return(sqlcall)
+  return(sqlcall)
 }
