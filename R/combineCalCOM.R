31a8148bf6fa82a50c80c682784a968abf343f2f
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2023-04-11 08:32:46 -0700

    Change assignment operator from = to <-

--- R/combineCalCOM.R
+++ R/combineCalCOM.R
@@ -35,7 +35,7 @@
 #' @seealso
 #' * [cleanPacFIN()], which should be ran after this function
 
-combineCalCOM = function ( Pdata, CalCOM ) {
+combineCalCOM <- function(Pdata, CalCOM) {
 
   cat( "\nCombining CalCOM and PacFIN data \n\n" )
 
@@ -44,51 +44,51 @@ combineCalCOM = function ( Pdata, CalCOM ) {
   cat( paste("PacFIN records:", nrow(Pdata)), "\n\n" )
   cat( paste("CalCOM records:", nrow(CalCOM)), "\n\n" )
 
-  CalCOM$SAMPLE_DATE = as.character(CalCOM$SAMPLE_DATE)
+  CalCOM$SAMPLE_DATE <- as.character(CalCOM$SAMPLE_DATE)
 
   # Break out Year, Month, Day from vector formatted "2/23/2012"
 
-  trueDate = as.Date(CalCOM$SAMPLE_DATE, format="%m/%d/%Y")
+  trueDate <- as.Date(CalCOM$SAMPLE_DATE, format="%m/%d/%Y")
 
-  CalCOM$SAMPLE_YEAR = as.numeric(format(trueDate, format="%Y"))
-  CalCOM$SAMPLE_MONTH = as.numeric(format(trueDate, format="%m"))
-  CalCOM$SAMPLE_DAY = as.numeric(format(trueDate, format="%d"))
+  CalCOM$SAMPLE_YEAR <- as.numeric(format(trueDate, format = "%Y"))
+  CalCOM$SAMPLE_MONTH <- as.numeric(format(trueDate, format = "%m"))
+  CalCOM$SAMPLE_DAY <- as.numeric(format(trueDate, format = "%d"))
 
   # Fix Areas
 
-  CalCOM$PSMFC_AREA = NA
-  CalCOM$PSMFC_AREA[CalCOM$PORT %in% c("ERK","CRS")] = "1C"
-  CalCOM$PSMFC_AREA[CalCOM$PORT %in% c("BRG","OSF","MNT","1")] = "1B"
-  CalCOM$PSMFC_AREA[CalCOM$PORT %in% c("OSB","MRO","2")] = "1A"
+  CalCOM$PSMFC_AREA <- NA
+  CalCOM$PSMFC_AREA[CalCOM$PORT %in% c("ERK","CRS")] <- "1C"
+  CalCOM$PSMFC_AREA[CalCOM$PORT %in% c("BRG","OSF","MNT","1")] <- "1B"
+  CalCOM$PSMFC_AREA[CalCOM$PORT %in% c("OSB","MRO","2")] <- "1A"
 
   # Create PacFIN format matrix and fill
 
-  Cal.dat = as.data.frame(matrix(data=NA, nrow = nrow(CalCOM) , ncol = ncol(Pdata)))
+  Cal.dat <- as.data.frame(matrix(data=NA, nrow = nrow(CalCOM) , ncol = ncol(Pdata)))
 
   names(Cal.dat) = names(Pdata)
 
-  Cal.dat$SPID         = CalCOM$SPECIES
-  Cal.dat$SAMPLE_NO    = CalCOM$SAMPLE_NO
-  Cal.dat$FISH_NO      = as.numeric(CalCOM$FISH_NO)
-  Cal.dat$FISH_LENGTH  = as.numeric(CalCOM$TLENGTH)
-  Cal.dat$SEX          = CalCOM$SEX
-  Cal.dat$DEPTH_AVG    = as.numeric(CalCOM$DEPTH)
-  Cal.dat$TOTAL_WGT    = as.numeric(CalCOM$TOTAL_WGT)
-  Cal.dat$SPECIES_WGT  = as.numeric(CalCOM$SumOfWEIGHT)
-  Cal.dat$PORT         = CalCOM$PORT_COMPLEX
-  Cal.dat$SAMPLE_YEAR  = as.numeric(CalCOM$SAMPLE_YEAR)
-  Cal.dat$SAMPLE_MONTH = as.numeric(CalCOM$SAMPLE_MONTH)
-  Cal.dat$SAMPLE_DAY   = as.numeric(CalCOM$SAMPLE_DAY)
-  Cal.dat$SOURCE_AGID  = "CalCOM"
-  Cal.dat$PSMFC_ARID   = "CalCOM"
-  Cal.dat$age1         = as.numeric(CalCOM$AGE)
-  Cal.dat$FISH_AGE_YEARS_FINAL = as.numeric(CalCOM$AGE)
-  Cal.dat$FREQ         = 1
-  Cal.dat$FISH_LENGTH_TYPE = ""
+  Cal.dat$SPID         <- CalCOM$SPECIES
+  Cal.dat$SAMPLE_NO    <- CalCOM$SAMPLE_NO
+  Cal.dat$FISH_NO      <- as.numeric(CalCOM$FISH_NO)
+  Cal.dat$FISH_LENGTH  <- as.numeric(CalCOM$TLENGTH)
+  Cal.dat$SEX          <- CalCOM$SEX
+  Cal.dat$DEPTH_AVG    <- as.numeric(CalCOM$DEPTH)
+  Cal.dat$TOTAL_WGT    <- as.numeric(CalCOM$TOTAL_WGT)
+  Cal.dat$SPECIES_WGT  <- as.numeric(CalCOM$SumOfWEIGHT)
+  Cal.dat$PORT         <- CalCOM$PORT_COMPLEX
+  Cal.dat$SAMPLE_YEAR  <- as.numeric(CalCOM$SAMPLE_YEAR)
+  Cal.dat$SAMPLE_MONTH <- as.numeric(CalCOM$SAMPLE_MONTH)
+  Cal.dat$SAMPLE_DAY   <- as.numeric(CalCOM$SAMPLE_DAY)
+  Cal.dat$SOURCE_AGID  <- "CalCOM"
+  Cal.dat$PSMFC_ARID   <- "CalCOM"
+  Cal.dat$age1         <- as.numeric(CalCOM$AGE)
+  Cal.dat$FISH_AGE_YEARS_FINAL <- as.numeric(CalCOM$AGE)
+  Cal.dat$FREQ         <- 1
+  Cal.dat$FISH_LENGTH_TYPE <- ""
   # IGT: FISH_LENGTH_UNITS = "MM" were added outside this function for Petrale 
   # in 2023, but I don't know if all CalCOM data are in MM
   if ("FISH_LENGTH_UNITS" %in% names(CalCOM)) {
-    Cal.dat$FISH_LENGTH_UNITS = CalCOM$FISH_LENGTH_UNITS
+    Cal.dat$FISH_LENGTH_UNITS <- CalCOM$FISH_LENGTH_UNITS
   }
 
   # No sample type or method, no INPFC_AREA, so give them values that are
@@ -96,17 +96,17 @@ combineCalCOM = function ( Pdata, CalCOM ) {
 
   # IGT: assigning surface but 1985-1989 samples in 2019 Petrale 
   # were assigned a combination of surface and break-and-burn
-  Cal.dat$AGE_METHOD1 = "S" 
-  Cal.dat$SAMPLE_METHOD = "R"
-  Cal.dat$SAMPLE_TYPE = "M"
+  Cal.dat$AGE_METHOD1 <- "S" 
+  Cal.dat$SAMPLE_METHOD <- "R"
+  Cal.dat$SAMPLE_TYPE <- "M"
 #  Cal.dat$INPFC_AREA = "CalCOM" # IGT: not present in PacFIN table 15 March 2023
 
-  Cal.dat$GRID = "CalCOM"
+  Cal.dat$GRID <- "CalCOM"
 
   # Fix SEX.  2=females, 1=males based on length distributions
 
-  Cal.dat$SEX[Cal.dat$SEX=="1"] = "M"
-  Cal.dat$SEX[Cal.dat$SEX=="2"] = "F"
+  Cal.dat$SEX[Cal.dat$SEX == "1"] <- "M"
+  Cal.dat$SEX[Cal.dat$SEX == "2"] <- "F"
 
   # Done.
   cat( paste("Combined dataset:", nrow(rbind(Pdata, Cal.dat)), "\n\n" ) )
