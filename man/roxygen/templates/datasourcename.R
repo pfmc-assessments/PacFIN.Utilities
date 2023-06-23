16b0c30e63d6c9dfd9b4dbb72ba13ed2e7fe3bd3
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2023-06-23 06:27:36 -0700

    fix(getDB): deprecate datasourcename for database
    Deprecates the input argument datasourcename in favor of database.
    datasourcename was not intuitive upon reading it, where database is
    slightly more suggestive that you should enter the name of the
    database. Also, datasourcename is seen by spell checkers as an
    incorrectly spelled word, where I am working on removing all
    camel-case variables in favor of snake case ... but I do not even know
    what you would call datasourcename in terms of formatting besides
    terrible :rofl:.

--- man/roxygen/templates/datasourcename.R
+++
@@ -1,3 +0,0 @@
-#' @param datasourcename The sql data base name that is stored on your
-#' computer. The default is \code{"PacFIN"} which will pull data from
-#' the Pacific Fisheries Information Network.
