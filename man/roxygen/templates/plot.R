12ea84ccf436d7ce5f6a6a50ca63391d4bd44914
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2024-12-29 12:32:48 -0800

    document

--- man/roxygen/templates/plot.R
+++ man/roxygen/templates/plot.R
@@ -1,4 +1,5 @@
-#' @param plot Argument takes either a logical or character value specifying the file
+#' @param plot Deprecated as of version 0.2.10 and replaced with savedir. 
+#'   Argument takes either a logical or character value specifying the file
 #'   name if you want to write the plots to a disk rather than printing them
 #'   to the console. If \code{plot = FALSE} no plots will be generated.
 #'   If printing to the disk the character value should end in \code{.png}
